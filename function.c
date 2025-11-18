81d8986b807f49e2274ee5e1fa5d38d9e694a170
laja99 <layla.jaber.m@gmail.com>
2025-11-18 03:03:13 +0300

    Add _putchar, print_string, and print_number functions

--- function.c
+++ function.c
@@ -1,13 +1,41 @@
 #include "main.h"
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+    return (write(1, &c, 1)); // write(int __fd, const void *__buf, size_t __n) fd (1) = write
 }
 int print_string(char *str)
 {
-    return (write(1, &str, 1));
+    int count = 0;
+    if (!str) //if null
+    {
+        str = "(null)";
+    }
+    while (*str) //print it and return number of string
+    {
+        _putchar(*str);
+        str++;
+        count++;
+    }
+    return(count);
 }
 int print_number(int n)
 {
-    return (write(1, &n, 1));
+    unsigned int n1 = 0;
+    int count = 0;
+if (n < 0) // if the number nigative make it positive 
+	{
+		count += _putchar('-');
+		n1 = -n;
+	}
+	else
+	{
+		n1 = n; // copy 
+	}
+
+	if (n1 >= 10) // more then one digit
+    {
+        count += print_number(n1 / 10); // recursion to print left ot right and count all digit العشرات
+    }
+	count += _putchar((n1 % 10) + '0'); ///transformation int to charالاحاد 
+	return (count);
 }

f0a1e88656c08ae35ddab837d153bc2e8d5eb127
laja99 <layla.jaber.m@gmail.com>
2025-11-18 03:37:26 +0300

    Organize main.h and declare helper functions for custom _printf implementation

--- main.h
+++ main.h
@@ -4,13 +4,13 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);  // Function that prints formatted output, replacing % with corresponding arguments
-int _putchar(char c); // %c
-int print_string(char *str); // %s
-int print_number(int n); // %d or %i
+int _printf(const char *format, ...);  /* Function that prints formatted output, replacing % with corresponding arguments*/
+int _putchar(char c); /* %c */
+int print_string(char *str); /* %s */
+int print_number(int n); /* %d or %i */
 
 int print_unsigned(unsigned int n);
-int _octal(unsigned int n); // %o
-int _hexadecimal(unsigned int n, int uppercase); // %X or %x 
+int _octal(unsigned int n); /* %o */
+int _hexadecimal(unsigned int n, int uppercase); /* %X or %x */
 
 #endif

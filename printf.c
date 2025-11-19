16c8b090a9cd4c46661773d7424d315ca7426bf7
laja99 <layla.jaber.m@gmail.com>
2025-11-19 12:35:13 +0300

    Improved _printf: replaced index with pointer for cleaner logic, added automatic newline at end of output, and documented functions for Betty style compliance.

--- printf.c
+++ printf.c
@@ -1,48 +1,48 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+/**
+* _printf - function that produces output according to a format
+* @format: format string containing format spesifiers
+* Return: number of digit printed
+*/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int count = 0; /* for return number of byte printed*/
+	va_list args;
+	int count = 0; /* for return number of byte printed*/
 
-    if (!format || format[0] == '%' || format[1] == '\0') /*if format NULL or % in program name */
-    {
-        return (-1);
-    }
+	if (!format) /*if format NULL */
+	{
+		return (-1);
+	}
 
-    va_start (args, format); 
-    while (format[i])
-    {
-        if (format[i] == '%') /* if we find % look to anther letter*/
-        {
-	        i++;
-	        if (format[i] == '\0')
-            {
-	        	return (-1);
-	        }
-            else if (format[i] == 'c')
-            {
-                count += _putchar(va_arg(args, int));
-            }
-            else if (format[i] == 's')
-            {
-                count += print_string(va_arg(args, char*));
-            }
-            else if (format[i] == '%')
-            {
-                count += _putchar('%');
-            }
-        }else
-        {
-            count += _putchar(format[i]);
-            i++;
-        }
-    
-    }
-    
-    va_end(args);
-    _putchar('\n');
-    return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%') /* if we find % look to next letter*/
+		{
+			++format;
+			if (!*format)
+				return (-1);
+			else if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(va_arg(args, char*));
+			else if (*format == '%')
+				count += _putchar('%'); /* or count += _putchar(*format) */
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else /*Print the character next to '%' if it hasn't in the code */
+		{
+			count += _putchar(*format);
+		}
+		++format;
+	}
+	count += _putchar('\n');
+	va_end(args);
+	return (count);
 }

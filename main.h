f1fc039ed14bda53302f0a204ed8f48ed753b928
MohammedError <mohammed.secure@outlook.sa>
2025-11-21 17:05:20 +0000

    Fix header and implement correct va_list logic for all tasks

--- main.h
+++ main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <unistd.h>
-
-int _printf(const char *format, ...);
-int print_int(int n);
+#include <stdlib.h>
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list);
+} print_t;
 int _putchar(char c);
-
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
 #endif
